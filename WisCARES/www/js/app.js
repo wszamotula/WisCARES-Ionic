6581d76bf7c155969a6074e2f5c19c94fce9c218
Exlumberjack <rwturner@wisc.edu>
2015-11-01 14:47:27 -0600

    Added cookie support to Angular's http

--- WisCARES/www/js/app.js
+++ WisCARES/www/js/app.js
@@ -25,12 +25,13 @@ angular.module('wiscares', ['ionic', 'wiscares.controllers', 'wiscares.services'
 })
 
 .config(function ($stateProvider, $urlRouterProvider) {
-
+    $httpProvider.defaults.withCredentials = true;
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider
+
 
     // setup an abstract state for the tabs directive
       .state('tab', {
