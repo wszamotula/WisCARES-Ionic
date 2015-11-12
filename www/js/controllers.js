8570386bf8860b5bf05108351367c4cee07d681f
cguo42 <guochang19921201@gmail.com>
2015-11-12 03:49:16 -0600

    add camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -96,6 +96,113 @@ angular.module('wiscares.controllers', ['ui.router'])
   $scope.loadPet(); // Load a movie which can be edited on UI
 })
 
+.controller('CameraCtrl', ['$scope', "CameraPopover", "$ionicActionSheet", function ($scope, CameraPopover, $ionicActionSheet) {
+    $scope.showProgress = false;
+
+    var uploadFileUrl = "serve api";
+
+    $scope.showActionSheet = function () {
+        // Show the action sheet
+        var hideSheet = $ionicActionSheet.show({
+            buttons: [
+              { text: 'Take Photo' },
+              { text: 'Take Phone from albums' }
+            ],
+            // destructiveText: 'Delete',
+            titleText: 'Select photos from',
+            cancelText: 'Cancel',
+            cancel: function () {
+                hideSheet();
+            },
+            buttonClicked: function (index) {
+                // click "take phone"
+                if (index == 0) {
+                    takePicture({
+                        quality: 100,
+                        allowEdit: true,
+                        targetWidth: 500,
+                        targetHeight: 225,
+                        // Android doesn't recognize this.
+                        // http://stackoverflow.com/questions/29392639/error-capturing-image-with-phonegap-on-android
+                        // saveToPhotoAlbum: true,
+                        sourceType: Camera.PictureSourceType.CAMERA,
+                        encodingType: Camera.EncodingType.JPEG,
+                        destinationType: Camera.DestinationType.FILE_URI
+                    });
+                } else if (index == 1) {
+                    takePicture({
+                        quality: 100,
+                        allowEdit: true,
+                        targetWidth: 500,
+                        targetHeight: 225,
+                        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                        encodingType: Camera.EncodingType.JPEG,
+                        destinationType: Camera.DestinationType.FILE_URI
+                    });
+                } else {
+                    return true;
+                }
+                hideSheet();
+            }
+        });
+    };
+
+    // upload file with a imageURI
+    var uploadFile = function (imageURI) {
+        // show the progress bar
+        safeApply($scope, function () {
+            $scope.showProgress = true;
+        });
+        var uploadOptions = new FileUploadOptions();
+        uploadOptions.fileKey = "petImage";
+        uploadOptions.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+        uploadOptions.mimeType = "image/jpeg";
+        uploadOptions.chunkedMode = false;
+
+        var ft = new FileTransfer();
+
+        var statusDom = document.getElementById("ft-prog");
+
+        ft.onprogress = function (progressEvent) {
+            if (progressEvent.lengthComputable) {
+                var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+                statusDom.value = perc;
+                if (perc == 100) {
+                    safeApply($scope, function () {
+                        $scope.showProgress = false;
+                    });
+                }
+            } else {
+                console.log("loading....");
+            }
+        };
+
+        ft.upload(imageURI, encodeURI(uploadFileUrl), onSuccess, onFail, uploadOptions, true);
+
+        function onSuccess(responseData) {
+            responseString = JSON.stringify(responseData);
+            responseObject = JSON.parse(responseString);
+            responsePerson = JSON.parse(responseObject.response);
+            safeApply($scope, function () {
+                // update url
+            });
+        };
+        function onFail() {
+            alert("something wrong, please try again");
+        };
+    };
+
+    //get photos form device and return a file path url
+    var takePicture = function (options) {
+        CameraPopover.getPicture(options).then(function (imageURI) {
+            uploadFile(imageURI);
+        }, function (err) {
+            console.error(err);
+        });
+    };
+
+}])
+
 .controller('VetsCtrl', function ($scope, Vets) {
     var userID = window.localStorage['userId'];
     console.log(userID);
